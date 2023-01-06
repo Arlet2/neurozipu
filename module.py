1cdce2b75a52c239ca7c9ee7e3b80a60fe5b6f3e
Arlet2 <shulga@niuitmo.ru>
2023-01-07 00:24:56 +0300

    Add bot to child modules

--- module.py
+++ module.py
@@ -1,9 +1,12 @@
+import telebot
 class Module:
     name: str
     description: str
     action = ()
+    bot: telebot.TeleBot
 
-    def __init__(self, name: str, description: str, action) -> None:
+    def __init__(self, name: str, description: str, action, bot: telebot.TeleBot) -> None:
         self.name = name
         self.description = description
         self.action = action
+        self.bot = bot
