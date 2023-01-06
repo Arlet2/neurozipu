658a2ebe2c8d7945ceabcd3695a68fe6d08ea76b
Arlet2 <shulga@niuitmo.ru>
2023-01-07 00:24:38 +0300

    Create simple menu work

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from menu import Menu
+import telebot
+import os
 
 def main():
-    menu = Menu()
+    bot = create_bot()
+
+    menu = Menu(bot)
 
     while (menu() != 0):
-        pass
+       pass
+
+def create_bot() -> telebot.TeleBot:
+    token = os.environ["NEUROZIPU_TOKEN"]
+    return telebot.TeleBot(token)
+
+
 
 main()
